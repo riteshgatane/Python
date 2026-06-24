ebbcc8391e3dfdef676f086fa622fa1ee4bf88dc
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-24 22:25:25 +0530

    Add files via upload

---
+++ Typedemo.py
@@ -0,0 +1,19 @@
+#Numeric 
+no = 11        #int 
+Marks = 90.78  #float
+Value = 8+5j   #Complex
+
+print(Value)
+print(type(no))
+print(type(Marks))
+print(type(Value))
+
+
+#Boolean 
+Gender = True
+print(Gender)
+print(type(Gender))
+
+Demo = None 
+print(Demo)
+print(type(Demo))
